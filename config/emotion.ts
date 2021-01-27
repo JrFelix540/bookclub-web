903bac012524d58d977aa71768a5a3b7d6425d63
Junior Felix <juniorfelix8825@outlook.com>
2021-01-27 13:24:04 +0300

    🐛 BUG: Removes emotion configuration

--- config/emotion.ts
+++ config/emotion.ts
@@ -2,4 +2,4 @@ import styled, { CreateStyled } from "@emotion/styled";
 
 import theme from "~/styles/theme";
 
-export default styled as CreateStyled<typeof theme>;
+// export default styled as CreateStyled<typeof theme>;
