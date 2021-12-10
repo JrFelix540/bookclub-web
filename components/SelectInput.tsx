2c511f418b7ccb7c5599d68972a926e7b9d950a7
Junior Felix <juniorfelix8825@outlook.com>
2021-12-10 16:57:29 +0300

    🐛 BUG: Fixes build

--- components/SelectInput.tsx
+++ components/SelectInput.tsx
@@ -1,47 +1,44 @@
-import {
-    FormControl,
-    FormErrorMessage,
-    Select,
-} from "@chakra-ui/react";
+import { FormControl, FormErrorMessage, Select } from "@chakra-ui/react";
 import { useField } from "formik";
 import React, { Fragment, SelectHTMLAttributes } from "react";
 
 interface ICommunity {
-    id: number;
-    name: string;
+  id: number;
+  name: string;
 }
 
 type SelectInputProps = SelectHTMLAttributes<HTMLSelectElement> & {
-    name: string;
-    label: string;
-    optionValues: ICommunity[];
-    size?: string;
+  name: string;
+  label: string;
+  optionValues: ICommunity[];
+  size?: string;
 };
 
-const SelectInput: React.FC<SelectInputProps> = (props) => {
-    const [field, { error }] = useField(props);
+const SelectInput: React.FC<SelectInputProps> = ({
+  optionValues,
+  ...props
+}) => {
+  const [field, { error }] = useField(props);
 
-    return (
-        <Fragment>
-            <FormControl isInvalid={!!error}>
-                <Select
-                    placeholder={props.label}
-                    {...field}
-                    id={field.name}
-                    {...props}
-                >
-                    {props.optionValues?.map((val) => (
-                        <option value={val.id} key={val.name}>
-                            {val.name}
-                        </option>
-                    ))}
-                </Select>
-                {error && (
-                    <FormErrorMessage>{error}</FormErrorMessage>
-                )}
-            </FormControl>
-        </Fragment>
-    );
+  return (
+    <Fragment>
+      <FormControl isInvalid={!!error}>
+        <Select
+          placeholder={props.label}
+          {...field}
+          id={field.name}
+          // {...props}
+        >
+          {optionValues?.map((val) => (
+            <option value={val.id} key={val.name}>
+              {val.name}
+            </option>
+          ))}
+        </Select>
+        {error && <FormErrorMessage>{error}</FormErrorMessage>}
+      </FormControl>
+    </Fragment>
+  );
 };
 
 export default SelectInput;
