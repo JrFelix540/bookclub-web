40646f907a8c843916540c7c2358fb0dd4339a40
Junior Felix <juniorfelix8825@outlook.com>
2021-12-11 14:31:43 +0300

    Changes grey color

--- styles/theme.ts
+++ styles/theme.ts
@@ -2,45 +2,45 @@ import { extendTheme } from "@chakra-ui/react";
 import { createBreakpoints, mode } from "@chakra-ui/theme-tools";
 
 const styles = {
-    global: (props) => ({
-        body: {
-            color: mode("blue.500", "whiteAlpha.900")(props),
-            bg: mode("gray.100", "#141214")(props),
-        },
-    }),
+  global: (props) => ({
+    body: {
+      color: mode("blue.500", "whiteAlpha.900")(props),
+      bg: mode("gray.100", "#141214")(props),
+    },
+  }),
 };
 
 const breakpoints = createBreakpoints({
-    sm: "36em",
-    md: "48em",
-    lg: "62em",
-    xl: "75em",
+  sm: "36em",
+  md: "48em",
+  lg: "62em",
+  xl: "75em",
 });
 const overrides = {
-    colors: {
-        purple: {
-            500: "#8257e5",
-        },
-        blue: {
-            500: "#0f3057",
-            400: "#00587a",
-            300: "#008891",
-        },
-        gray: {
-            300: "#e1e1e6",
-            500: "#e7e7de",
-            600: "#29292e",
-            700: "#202024",
-            800: "#121214",
-        },
+  colors: {
+    purple: {
+      500: "#8257e5",
     },
-    fonts: {
-        body: "Roboto, system-ui, sans-serif",
-        heading: "Roboto, system-ui, sans-serif",
-        mono: "Menlo, monospace",
+    blue: {
+      500: "#0f3057",
+      400: "#00587a",
+      300: "#008891",
     },
-    breakpoints,
-    styles,
+    gray: {
+      300: "#e1e1e6",
+      500: "#e7e7de",
+      600: "#F0F0F0",
+      700: "#202024",
+      800: "#121214",
+    },
+  },
+  fonts: {
+    body: "Roboto, system-ui, sans-serif",
+    heading: "Roboto, system-ui, sans-serif",
+    mono: "Menlo, monospace",
+  },
+  breakpoints,
+  styles,
 };
 const customTheme = extendTheme(overrides);
 
